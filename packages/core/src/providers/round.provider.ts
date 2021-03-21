bcdccc81a996b14ec384a25cd277dd8dfa547cec
corentin verquin <corentin.verquin@gmail.com>
2021-03-21 15:54:30 +0100

    feat(core/tournament): get round use case

--- packages/core/src/providers/round.provider.ts
+++ packages/core/src/providers/round.provider.ts
@@ -2,4 +2,5 @@ import { Round } from '@cph-scorer/model'
 
 export interface RoundProvider{
   insert: (roundNumber: number) => Promise<Round>
+  getRound: (roundNumber: number) => Promise<Round>
 }
