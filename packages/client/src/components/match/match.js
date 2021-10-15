d4070c945d2d357484105808aa4310e2614e0e31
corentin verquin <corentin.verquin@gmail.com>
2021-10-15 12:10:23 +0200

    feat(client/round): show round

---
+++ packages/client/src/components/match/match.js
@@ -0,0 +1,6 @@
+export default function () {
+    Alpine.data('match', () => ({
+        area : [1,3,6,8,2,4,5,7,9],
+
+    }))
+}
---
+++ packages/client/src/static/js/store/round.js
@@ -0,0 +1,10 @@
+export default function () {
+    Alpine.store('RoundStore', {
+        init() {
+            this.round = {}
+        },
+        create(round) {
+            this.round = {...round}
+        }
+    })
+}
