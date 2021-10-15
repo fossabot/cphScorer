970ccc795b72ac54b222bb153f6202c9ae278f7b
corentin verquin <corentin.verquin@gmail.com>
2021-10-15 11:00:41 +0200

    feat(client/index): generate round

--- packages/client/src/static/js/index.js
+++ packages/client/src/static/js/index.js
@@ -3,13 +3,40 @@ import Alpine from 'alpinejs'
 import RegisterStore from './store/register'
 import RegisterInput from "../../components/register-input/register-input";
 import Register from "../../components/register/register";
+import { post } from "./util/client";
+import { ROUND } from "./util/route";
+import notification from "./util/notification";
 
 window.Alpine = Alpine
 
-document.addEventListener('alpine:init', () => {    
+function loadStore() {
     RegisterStore()
+}
+
+function loadComponent() {
     RegisterInput()
     Register()
+}
+
+function loadPage() {
+    const NUMBER_OF_ROUND = 3
+
+    Alpine.data('index', () => ({
+        buttonDisabled: false,
+
+        async start() {
+            this.buttonDisabled = true
+            await post(ROUND.CREATE(NUMBER_OF_ROUND), {})
+            notification('success', 'Parties correctement générés !')
+            this.buttonDisabled = false
+        }
+    }))
+}
+
+document.addEventListener('alpine:init', () => {
+    loadStore()
+    loadComponent()
+    loadPage()
 })
 
 Alpine.start()
